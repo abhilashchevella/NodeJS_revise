42970eb474a2cb205bd3ca55d4c867c3602389bf
abhilashchevella <abhilashchevella@gmail.com>
2025-07-27 23:56:23 +0530

    basics

---
+++ JS Basics/Basic_dataTypes.js
@@ -0,0 +1,14 @@
+// let
+let counter = 2;
+console.log('1st print count: ', counter);
+
+counter += 1;
+console.log('2st print count: ', counter);
+
+// interger
+const a = '123'; 
+console.log('int: ', a);
+
+const b = '1.314'
+console.log("pie float: ", b);
+
