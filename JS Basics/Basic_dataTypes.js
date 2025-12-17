ba591b0f2ca71aa4ece7aec9d7dd96a98bf680b3
abhilashchevella <abhilashchevella@gmail.com>
2025-12-17 11:03:32 +0530

    dataTypes revision

--- JS Basics/Basic_dataTypes.js
+++ JS Basics/Basic_dataTypes.js
@@ -1,14 +1,29 @@
-// let
-let counter = 2;
-console.log('1st print count: ', counter);
+// // let
+// let counter = 2;
+// console.log('1st print count: ', counter);
 
-counter += 1;
-console.log('2st print count: ', counter);
+// counter += 1;
+// console.log('2st print count: ', counter);
 
-// interger
-const a = '123'; 
-console.log('int: ', a);
+// // interger
+// const a = '123'; 
+// console.log('int: ', a);
 
-const b = '1.314'
-console.log("pie float: ", b);
+// const b = '1.314'
+// console.log("pie float: ", b);
 
+
+// Node.js program to demonstrate the 
+// process.stdin Property 
+
+// Enter any texts ( User input)
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
